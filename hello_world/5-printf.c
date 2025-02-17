c9ff1b0a24c92088d6ad7714d493a4fce65eb78b
chems33 <chemsorba@yahoo.com>
2025-02-17 10:12:50 +0000

    oui

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**                                                                                                                                                                                            
+ * main - Entry point of the program                                                                                                                                                           
+ *                                                                                                                                                                                             
+ * Description: This program prints the string                                                                                                                                                 
+ * "Programming is like building a multilingual puzzle"                                                                                                                                        
+ * followed by a new line using the `puts` function.                                                                                                                                           
+ *                                                                                                                                                                                             
+ * Return: Always 0 (indicating successful execution)                                                                                                                                          
+ */
+
 int main(void)
 {
-    printf("Art,\n");
+    printf("with proper grammar, but the outcome is a piece of art,");
     return 0;
 }
