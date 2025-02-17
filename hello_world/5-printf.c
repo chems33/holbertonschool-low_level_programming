4b890f4f17037d54bf150153461705f88919e16b
chems33 <chemsorba@yahoo.com>
2025-02-17 10:47:11 +0000

    oui

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/**                                                                                                                                                                                            
- * main - Entry point of the program                                                                                                                                                           
- *                                                                                                                                                                                             
- * Return: Always 0 (Success)                                                                                                                                                                  
+/**
+ *main
+ *
+ *Return 0
  */
+
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
