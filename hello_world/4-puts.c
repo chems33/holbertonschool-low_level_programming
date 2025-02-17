641023019b1a32413988c4f0ddccf93c7220ec91
chems33 <chemsorba@yahoo.com>
2025-02-17 10:02:50 +0000

    oui

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -9,8 +9,9 @@
  *
  * Return: Always 0 (indicating successful execution)
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
