87668acba9f849f83846a3d43f3063648c22a373
chems33 <chemsorba@yahoo.com>
2025-02-17 09:43:53 +0000

    oji

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the string
+ * "Programming is like building a multilingual puzzle"
+ * followed by a new line using the `puts` function.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
